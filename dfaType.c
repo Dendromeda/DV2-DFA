e41ee4ac3447b88d35cdb4c19a8a8c979b91cca1
Lidngren <adamlindgren@outlook.com>
2018-02-28 09:31:34 +0100

    Lagt till kommentarer, fixat struktur
    Lagt till funktionskommentar till stringcmp. Städat upp strukturen i C-filen.

--- dfaType.c
+++ dfaType.c
@@ -40,7 +40,7 @@ bool stringcmp(void *p1, void *p2){
 size_t hashFunc(void *key){
 	int e = 0;
 	char *str = key;
-	for (int i = 0; str[i]; i++){
+	for (int i = 0; str[i]; i++) {
 			e += str[i];
 	}
 	return e;
@@ -55,69 +55,6 @@ static char *nodeGetLabel(dfaNode *n){
 	return n->label;
 }
 
-/* Function:	getNode
- * Description:	Returns the node corresponding to the given string.
- * Input:		dfaNode, string
- * Output:		dfaNode
- */
-static dfaNode *getNode(dfaType *dfa, char *str){
-	return table_lookup(dfa->nodes, str);
-}
-
-dfaType *dfa_init(size_t cap, char *start, size_t range){
-	dfaType *dfa = malloc(sizeof(dfaType));
-	dfa->range = range;
-	dfa->start = start;
-	dfa->accepted = table_empty(cap, &stringcmp, &hashFunc);
-	dfa->nodes = table_empty(cap, &stringcmp, &hashFunc);
-	return dfa;
-}
-
-void dfa_addNode(dfaType *dfa, char *label){
-	dfaNode *n = malloc(sizeof(dfaNode));
-	n->label = label;
-	n->connections = table_empty(dfa->range, stringcmp, hashFunc);
-	table_insert(dfa->nodes, (void*)label, n);
-}
-
-void dfa_addConnection(dfaType *dfa, char *orig, char *input, char *dest){
-	dfaNode *origNode = getNode(dfa, orig);
-	dfaNode *destNode = getNode(dfa, dest);
-	table_insert(origNode->connections, input, destNode);
-}
-
-void dfa_setAccepted(dfaType *dfa, char *label){
-	dfaNode *n = getNode(dfa, label);
-	table_insert(dfa->accepted, nodeGetLabel(n), n);
-}
-
-
-
-void dfa_kill(dfaType *dfa){
-	table_kill(dfa->nodes, *free, &freeNode);
-	table_kill(dfa->accepted, *free, NULL);
-
-}
-
-char *dfa_traverse(dfaType *dfa, char *orig, char *input){
-	dfaNode *origNode = getNode(dfa, orig);
-	dfaNode *destNode = table_lookup(origNode->connections, input);
-	if (destNode != NULL){
-		return nodeGetLabel(destNode);
-	} else {
-		printf("Va.\n");
-		return orig;
-	}
-}
-
-bool dfa_checkAccepted(dfaType *dfa, char *endNodeLabel){
-	if (table_lookup(dfa->accepted, endNodeLabel)){
-		return true;
-	} else {
-		return false;
-	}
-}
-
 /* Function:	nodeFree
  * Description:	frees all allocated memory in dfaNode structure
  * Input:		dfaNode
@@ -128,4 +65,71 @@ static void freeNode(void *n){
 	table_kill(node->connections, *free, *free);
 	free(node->label);
 	free(node);
+}
+
+/* Function:	getNode
+ * Description:	Returns the node corresponding to the given string.
+ * Input:		dfaNode, string
+ * Output:		dfaNode
+ */
+static dfaNode *getNode(dfaType *dfa, char *str){
+	return table_lookup(dfa->nodes, str);
+}
+
+
+dfaType *dfa_init(size_t cap, char *start, size_t range){
+	dfaType *dfa = malloc(sizeof(dfaType));
+	dfa->range = range;
+	dfa->start = start;
+	dfa->accepted = table_empty(cap, &stringcmp, &hashFunc);
+	dfa->nodes = table_empty(cap, &stringcmp, &hashFunc);
+	return dfa;
+}
+
+
+void dfa_addNode(dfaType *dfa, char *label){
+	dfaNode *n = malloc(sizeof(dfaNode));
+	n->label = label;
+	n->connections = table_empty(dfa->range, stringcmp, hashFunc);
+	table_insert(dfa->nodes, (void*)label, n);
+}
+
+
+void dfa_addConnection(dfaType *dfa, char *orig, char *input, char *dest){
+	dfaNode *origNode = getNode(dfa, orig);
+	dfaNode *destNode = getNode(dfa, dest);
+	table_insert(origNode->connections, input, destNode);
+}
+
+
+void dfa_setAccepted(dfaType *dfa, char *label){
+	dfaNode *n = getNode(dfa, label);
+	table_insert(dfa->accepted, nodeGetLabel(n), n);
+}
+
+
+void dfa_kill(dfaType *dfa){
+	table_kill(dfa->nodes, *free, &freeNode);
+	table_kill(dfa->accepted, *free, NULL);
+
+}
+
+
+char *dfa_traverse(dfaType *dfa, char *orig, char *input){
+	dfaNode *origNode = getNode(dfa, orig);
+	dfaNode *destNode = table_lookup(origNode->connections, input);
+	if (destNode != NULL) {
+		return nodeGetLabel(destNode);
+	} else {
+		return NULL;
+	}
+}
+
+
+bool dfa_checkAccepted(dfaType *dfa, char *endNodeLabel){
+	if (table_lookup(dfa->accepted, endNodeLabel)) {
+		return true;
+	} else {
+		return false;
+	}
 }
